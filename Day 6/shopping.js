7f1163fa89c9810ef38198834f55f66c925cf25b
Jiya04 <68240625+Jiya04@users.noreply.github.com>
2020-07-20 09:09:27 +0530

    assignment

---
+++ Day 6/shopping.js
@@ -0,0 +1,5 @@
+let shoppingList=[`vegetables`,`rice`,`sugar`,`spices`,`knife`,`glass`];
+console.log(shoppingList);
+let shoppingBasket=[...shoppingList,`notebook`,`pens`,`pencil`,`painting brush`,`oil paint`];
+console.log(shoppingBasket);
+
